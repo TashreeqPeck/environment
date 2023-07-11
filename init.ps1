f43618f0211fb8ad1d65a3203a32cc4caf67fff5
Tashreeq Peck <tashreeq.peck@gmail.com>
2023-07-12 06:15:47 +0800

    chore: disable unused dependency installs

--- init.ps1
+++ init.ps1
@@ -154,6 +154,45 @@ function Expand-Archive {
     }
 }
 
+function Install-Binary {
+    param(
+        $Name,
+        $FriendlyName = $Name,
+        $Uri,
+        $Extension = "zip",
+        $DestinationPath = $Name,
+        $EnvironmentPath = $Name
+    )
+    if (-Not (Get-Command -Name $Name -ErrorAction Ignore)) {
+        Write-Host "Installing $FriendlyName"
+        Invoke-WebRequest -Uri $Uri -OutFile "$TMPDirectory\$Name.$Extension"
+        Expand-Archive -Path "$TMPDirectory\$Name.$Extension" -DestinationPath "$env:LOCALAPPDATA\$DestinationPath"
+        Set-PathVariable -AddPath "$env:LOCALAPPDATA\$EnvironmentPath"
+    }
+    else {
+        Write-Host "$FriendlyName already installed"
+    }
+    Write-Host
+}
+
+function Install-WinGet {
+    param (
+        $Name,
+        $FriendlyName = $Name,
+        $Package
+    )
+
+    if (-Not (Get-Command -Name "$Name" -ErrorAction Ignore)) {
+        Write-Host "Installing $FriendlyName"
+        winget install --id $Package
+    }
+    else {
+        Write-Host "$FriendlyName already installed"
+    }
+    Write-Host
+    
+}
+
 # -------------------------------------------------------------------------------------------------
 # PowerShell 7
 # -------------------------------------------------------------------------------------------------
@@ -182,48 +221,99 @@ ForEach-Object {
 # -------------------------------------------------------------------------------------------------
 # Starship
 # -------------------------------------------------------------------------------------------------
-if (-Not (Get-Command -Name "starship" -ErrorAction Ignore)) {
-    Write-Host "Installing Starship"
-    winget install --id Starship.Starship
-}
-Write-Host "Adding Starship config...."
-New-Symlink -Path "$env:USERPROFILE\.config\starship.toml" -Target "$PSScriptRoot\starship\starship.toml"
+# Install-WinGet -Name "starship" -FriendlyName "Starship" -Package Starship.Starship
+# if (-Not (Get-Command -Name "starship" -ErrorAction Ignore)) {
+#     Write-Host "Installing Starship"
+#     winget install --id Starship.Starship
+# }
+# Write-Host "Adding Starship config...."
+# New-Symlink -Path "$env:USERPROFILE\.config\starship.toml" -Target "$PSScriptRoot\starship\starship.toml"
 
 # -------------------------------------------------------------------------------------------------
 # Neovim
 # -------------------------------------------------------------------------------------------------
-if (-Not (Get-Command -Name "nvim" -ErrorAction Ignore)) {
-    Write-Host "Installing Neovim"
-    winget install Neovim.Neovim
-}
-Write-Host "Adding Neovim config...."
+Install-WinGet -Name "nvim" -FriendlyName "Neovim" -Package Neovim.Neovim
+# if (-Not (Get-Command -Name "nvim" -ErrorAction Ignore)) {
+#     Write-Host "Installing Neovim"
+#     winget install Neovim.Neovim
+# }
+# Write-Host "Adding Neovim config...."
 New-Symlink SymbolicLink -Path "$env:LOCALAPPDATA\nvim" -Target "$PSScriptRoot\nvim"
 
 # -------------------------------------------------------------------------------------------------
 # lf
 # -------------------------------------------------------------------------------------------------
-if (-Not (Get-Command -Name "lf" -ErrorAction Ignore)) {
-    Write-Host "Installing lf"
-    Invoke-WebRequest -Uri https://github.com/gokcehan/lf/releases/download/r30/lf-windows-386.zip -OutFile "$TMPDirectory\lf.zip"
-    Expand-Archive -Path "$TMPDirectory\lf.zip" -DestinationPath "$env:LOCALAPPDATA\lf"
-    Set-PathVariable -AddPath "$env:LOCALAPPDATA\lf"
-}
-else {
-    Write-Host "lf already installed"
-}
+# Install-Binary -Name "lf" -Uri https://github.com/gokcehan/lf/releases/download/r30/lf-windows-386.zip
+# if (-Not (Get-Command -Name "lf" -ErrorAction Ignore)) {
+#     Write-Host "Installing lf"
+#     Invoke-WebRequest -Uri  -OutFile "$TMPDirectory\lf.zip"
+#     Expand-Archive -Path "$TMPDirectory\lf.zip" -DestinationPath "$env:LOCALAPPDATA\lf"
+#     Set-PathVariable -AddPath "$env:LOCALAPPDATA\lf"
+# }
+# else {
+#     Write-Host "lf already installed"
+# }
+# Write-Host
+
+# -------------------------------------------------------------------------------------------------
+# LazyGit
+# -------------------------------------------------------------------------------------------------
+# Install-Binary -Name "lazygit" -FriendlyName "LazyGit" -Uri https://github.com/jesseduffield/lazygit/releases/download/v0.38.2/lazygit_0.38.2_Windows_x86_64.zip
+# if (-Not (Get-Command -Name "lazygit" -ErrorAction Ignore)) {
+#     Write-Host "Installing LazyGit"
+#     Invoke-WebRequest -Uri https://github.com/jesseduffield/lazygit/releases/download/v0.38.2/lazygit_0.38.2_Windows_x86_64.zip -OutFile "$TMPDirectory\lazygit.zip"
+#     Expand-Archive -Path "$TMPDirectory\lazygit.zip" -DestinationPath "$env:LOCALAPPDATA\lazygit"
+#     Set-PathVariable -AddPath "$env:LOCALAPPDATA\lazygit"
+# }
+# else {
+#     Write-Host "LazyGit already installed"
+# }
+# Write-Host
 
 # -------------------------------------------------------------------------------------------------
 # gcc
 # -------------------------------------------------------------------------------------------------
-if (-Not (Get-Command -Name "gcc" -ErrorAction Ignore)) {
-    Write-Host "Installing MinGw64"
-    # Invoke-WebRequest -Uri https://github.com/niXman/mingw-builds-binaries/releases/download/13.1.0-rt_v11-rev1/x86_64-13.1.0-release-win32-seh-msvcrt-rt_v11-rev1.7z -OutFile "$TMPDirectory\mingw64.7z"
-    # Expand-Archive -Path "$TMPDirectory\mingw64.7z" -DestinationPath "$env:LOCALAPPDATA"
-    Set-PathVariable -AddPath "$env:LOCALAPPDATA\mingw64\bin"
-}
-else {
-    Write-Host "MinGw64 already installed"
-}
+# Install-Binary -Name "gcc" -FriendlyName "MinGw64" -Extension "7zip" -DestinationPath "" -EnvironmentPath "mingw64\bin" -Uri https://github.com/niXman/mingw-builds-binaries/releases/download/13.1.0-rt_v11-rev1/x86_64-13.1.0-release-win32-seh-msvcrt-rt_v11-rev1.7z
+# if (-Not (Get-Command -Name "gcc" -ErrorAction Ignore)) {
+#     Write-Host "Installing MinGw64"
+#     Invoke-WebRequest -Uri https://github.com/niXman/mingw-builds-binaries/releases/download/13.1.0-rt_v11-rev1/x86_64-13.1.0-release-win32-seh-msvcrt-rt_v11-rev1.7z -OutFile "$TMPDirectory\mingw64.7z"
+#     Expand-Archive -Path "$TMPDirectory\mingw64.7z" -DestinationPath "$env:LOCALAPPDATA"
+#     Set-PathVariable -AddPath "$env:LOCALAPPDATA\mingw64\bin"
+# }
+# else {
+#     Write-Host "MinGw64 already installed"
+# }
+
+# -------------------------------------------------------------------------------------------------
+# LuaRocks
+# -------------------------------------------------------------------------------------------------
+# Install-Binary -Name "luarocks" -FriendlyName "LuaRocks" -Uri http://luarocks.github.io/luarocks/releases/luarocks-3.9.2-windows-64.zip
+# if (-Not (Get-Command -Name "luarocks" -ErrorAction Ignore)) {
+#     Write-Host "Installing LuaRocks"
+#     Invoke-WebRequest -Uri https://github.com/gokcehan/lf/releases/download/r30/lf-windows-386.zip -OutFile "$TMPDirectory\luarocks.zip"
+#     Expand-Archive -Path "$TMPDirectory\luarocks.zip" -DestinationPath "$env:LOCALAPPDATA\luarocks"
+#     Set-PathVariable -AddPath "$env:LOCALAPPDATA\luarocks"
+# }
+# else {
+#     Write-Host "lf already installed"
+# }
+# Write-Host
+
+# -------------------------------------------------------------------------------------------------
+# Ruby 3.2
+# -------------------------------------------------------------------------------------------------
+# Install-WinGet -Name "ruby" -FriendlyName "Ruby 3.2" -Package "RubyInstallerTeam.Ruby.3.2"
+# Set-PathVariable -AddPath "C:\Ruby32-x64\bin"
+
+# -------------------------------------------------------------------------------------------------
+# PHP
+# -------------------------------------------------------------------------------------------------
+# Install-Binary -Name "php" -FriendlyName "PHP" -Uri https://windows.php.net/downloads/releases/php-8.2.7-Win32-vs16-x64.zip
+
+# -------------------------------------------------------------------------------------------------
+# Julia
+# -------------------------------------------------------------------------------------------------
+# Install-Binary -Name "julia" -EnvironmentPath "julia\julia-1.9.1\bin" -Uri https://julialang-s3.julialang.org/bin/winnt/x64/1.9/julia-1.9.1-win64.zip
 
 # -------------------------------------------------------------------------------------------------
 # Link PowerShell Profile
