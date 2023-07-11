b689687172fcd4139c6f90ee708fc508b77d697f
Tashreeq Peck <tashreeq.peck@gmail.com>
2023-07-12 07:42:59 +0800

    feat: set default tab settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,2 +1,12 @@
--- enable line number
-vim.opt.number = true
+local options = {
+    number = true, -- enable line numbers
+    cursorline = true, -- highlight the current line
+    expandtab = true, --convert tabs to spaces
+    shiftwidth = 4, -- number of spaces for each indent
+    tabstop = 4, -- insert 4 spaces for a tab
+}
+
+for key, value in pairs(options) do
+    vim.opt[key] = value
+end
+
