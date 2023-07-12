cee4bc274de907685382bcffed5f3f49fb058a3c
Tashreeq Peck <tashreeq.peck@gmail.com>
2023-07-12 20:51:05 +0800

    feat: add file tree plugin

---
+++ nvim/lua/plugins/nvimtree.lua
@@ -0,0 +1,17 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        disable_netrw = true,
+        hijack_netrw = true,
+        hijack_cursor = true,
+        hijack_unnamed_buffer_when_opening = false,
+        sync_root_with_cwd = true,
+    },
+    config = function(_, opts)
+        require("nvim-tree").setup(opts)
+    end,
+}
