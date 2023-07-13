e03e791d26c5a4ed622fdd6461da4179bb55c2f5
Tashreeq Peck <tashreeq.peck@gmail.com>
2023-07-13 18:36:38 +0800

    feat: configure nvimtree

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -8,10 +8,21 @@ return {
         require("utils").load_mappings("nvimtree")
     end,
     opts = {
+
         disable_netrw = true,
         hijack_netrw = true,
         hijack_cursor = true,
+        hijack_unnamed_buffer_when_opening = false,
         sync_root_with_cwd = true,
+        update_focused_file = {
+            enable = true,
+            update_root = false,
+        },
+        view = {
+            side = "left",
+            width = "20%",
+            preserve_window_proportions = false,
+        },
     },
     config = function(_, opts)
         require("nvim-tree").setup(opts)
